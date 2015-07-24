f907769b4e9fff1c90b95c1c67745d51a4e856f2
aakbar24 <makbar24@gmail.com>
2015-07-24 00:03:03 -0400

    adding new web app

---
+++ MCSD/MvcApplication2/Controllers/DisplayController.cs
@@ -0,0 +1,45 @@
+﻿using MvcApplication2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class DisplayController : Controller
+    {
+        //
+        // GET: /Display/
+
+        public string Index()
+        {
+            return "iiiii";
+        }
+        public string name()
+        {
+            return "this is from account controller NAME";
+        }
+
+        public ActionResult abc()
+        {
+            Student s = new Student();
+            Student s1 = new Student();
+            Student s2 = new Student();
+
+            s.name = "ovi";
+            s.age = 19;
+            s1.name = "ovi1";
+            s1.age = 191;
+            s2.name = "ovi2";
+            s2.age = 192;
+            
+            List<Student> students = new List<Student>();
+            students.Add(s);
+            students.Add(s1);
+            students.Add(s2);
+            return View(students);
+        }
+
+    }
+}
---
+++ MCSD/MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+           // ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+
+            return "HOW ARE YOU ... TO THE WORLD!!";
+        }
+        public string name()
+        {
+            return "this is from account controller NAME";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
