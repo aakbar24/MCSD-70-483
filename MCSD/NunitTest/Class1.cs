3162b9866003a9fc50f90140df70fb6fba03996e
aakbar24 <makbar24@gmail.com>
2016-02-23 02:06:49 -0500

    test

--- MCSD/NunitTest/Class1.cs
+++ MCSD/NunitTest/Class1.cs
@@ -15,7 +15,7 @@ namespace NunitTest
         public void positiveTest()
         {
             int x = 7;
-            test
+            //test
             int y = 8;
             Program p = new Program();
       
